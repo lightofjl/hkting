26e389207f30c967c3c5a688f743dc38ca99de3b
lightofjl <lightofjl@gmail.com>
2014-06-29 17:47:48 +0800

    test

--- src/com/example/android/displayingbitmaps/ui/ImageDetailFragment.java
+++ src/com/example/android/displayingbitmaps/ui/ImageDetailFragment.java
@@ -32,6 +32,7 @@ import com.example.android.displayingbitmaps.util.Utils;
 /**
  * This fragment will populate the children of the ViewPager from {@link ImageDetailActivity}.
  */
+//I have to do some change
 public class ImageDetailFragment extends Fragment {
     private static final String IMAGE_DATA_EXTRA = "extra_image_data";
     private String mImageUrl;
